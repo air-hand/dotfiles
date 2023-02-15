7045dcb3c1f28adf9ed5e5982a8e2e60dff69940
air-hand <45233435+air-hand@users.noreply.github.com>
2023-02-16 00:40:37 +0900

    avoid a issue of docker-credential-desktop.exe

---
+++ dot_local/bin/executable_fix-docker-desktop-slow.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "run $0"
+
+if ! command -v docker-credential-desktop.exe &> /dev/null; then
+    exit 0
+fi
+
+# avoid https://github.com/docker/for-win/issues/10247
+echo "delete credsStore key to ignore docker-credential-desktop.exe hangs..."
+
+if [ ! -f ~/.docker/config.json ]; then
+    exit 0
+fi
+
+if ! grep "credsStore" ~/.docker/config.json > /dev/null 2>&1; then
+    exit 0
+fi
+
+cat ~/.docker/config.json | grep -v "credsStore" > /tmp/fixed-docker-config.json
+mv -f /tmp/fixed-docker-config.json ~/.docker/config.json
+
