7e0da4cf692a9a801a15512b514849d1eeb2caff
air-hand <45233435+air-hand@users.noreply.github.com>
2023-03-25 23:56:22 +0900

    feat: install npiperelay automatically.

--- dot_local/bin/setup-socat-ssh-agent.sh
+++ dot_local/bin/setup-socat-ssh-agent.sh
@@ -1,11 +1,29 @@
 #!/usr/bin/env bash
 
+NPIPERELAY=npiperelay.exe
+
+install_npiperelay() {
+    local NPIPERELAY_ZIP=https://github.com/jstarks/npiperelay/releases/download/v0.1.0/npiperelay_windows_amd64.zip
+
+    mkdir -p ~/.local/bin && cd ~/.local/bin
+    # unzip doesnt support to read from stdin.
+    curl -sL -o npiperelay.zip $NPIPERELAY_ZIP
+    unzip -o npiperelay.zip $NPIPERELAY
+    rm npiperelay.zip
+    cd -
+}
+
 start_ssh_agent() {
     export SSH_AUTH_SOCK=$HOME/.ssh/agent.sock
 
+    command -v $NPIPERELAY &> /dev/null || install_npiperelay
+
     if ! ss -a | grep -q $SSH_AUTH_SOCK; then
         rm -f $SSH_AUTH_SOCK
-        (setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"npiperelay.exe -ei -s -v //./pipe/openssh-ssh-agent",nofork &) &> /dev/null
+        # prerequisite
+        # - Start-Service ssh-agent on Windows
+        # - ssh-add key on Windows
+        (setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"${NPIPERELAY} -ei -s -v //./pipe/openssh-ssh-agent",nofork &) &> /dev/null
     fi
 }
 
