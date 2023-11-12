2b76e594c10a885aa5df1980efb94d6d3ff91ca8
air-hand <45233435+air-hand@users.noreply.github.com>
2023-11-12 19:37:42 +0900

    feat: add watch_cmd for utility.

--- Makefile
+++ Makefile
@@ -2,6 +2,20 @@ MAKEFILE_DIR := $(dir $(abspath $(lastword $(MAKEFILE_LIST))))
 
 VSCODE_PATH := $(MAKEFILE_DIR)/vscode
 
+.DEFAULT := all
+
+MAKE := make --no-print-directory
+
+all: apply
+
+.PHONY: apply
+apply:
+	chezmoi apply
+
+.PHONY: watch
+watch:
+	/bin/bash -i -c 'EXCLUDE=1 watch_cmd "\.git\/" "$(MAKE)"'
+
 .PHONY: copy_from_current
 copy_from_current:
 	@VSCODE_USER_SPACE=$$(wslpath "$$(wslvar APPDATA)/Code/User") \
--- dot_local/bin/functions.sh
+++ dot_local/bin/functions.sh
@@ -3,3 +3,42 @@
 repo_root() {
     cd $(git rev-parse --show-toplevel)
 }
+
+watch_cmd() {
+    if [ $# -lt 2 ]; then
+        cat << EOF
+Usage
+
+${0} .*\.txt date
+${0} '.*\.(c|cc|cpp|h|hpp)$' make all
+
+# exclude file pattern
+EXCLUDE=1 ${0} '.*\.(c|cc|cpp|h|hpp)$' echo ignore_cpp_files
+
+# debug inotify events
+DEBUG=1 ${0} '.*\.(c|cc|cpp|h|hpp)$' make build
+
+EOF
+        return 1
+    fi
+    PATTERN="${1}"
+    PATTERN_OPTION="--include ${PATTERN}"
+    POST_COMMAND="${2}"
+
+    if [ -n "${EXCLUDE}" ]; then
+        PATTERN_OPTION="--exclude ${PATTERN}"
+    fi
+
+    if ! command -v inotifywait &> /dev/null; then
+        sudo apt install -y inotify-tools
+    fi
+
+    inotifywait -m -e modify -e create -e delete -r $(pwd) \
+        --format '%T %w%f %e' --timefmt '%FT%T' \
+        $PATTERN_OPTION | \
+        while read -r TIME FILEPATH EVENTS; do \
+            # debug inotify events
+            test ! -z $DEBUG && echo "T: ${TIME}, F: ${FILEPATH}, E: ${EVENTS}"; \
+            $POST_COMMAND; \
+        done
+}
