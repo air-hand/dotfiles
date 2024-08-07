d394d764c73aa093d8a0da38684a78011f2d5cbe
air-hand <45233435+air-hand@users.noreply.github.com>
2024-08-07 21:42:54 +0900

    fix: switch terraform to tofu.

--- .github/scripts/tfwrapper.sh
+++ .github/scripts/tfwrapper.sh
@@ -8,9 +8,9 @@ base_dir=$(git rev-parse --show-toplevel) # Please fix if necessary
 target=${PWD#"$base_dir"/}
 
 if [ "$command" == "plan" ]; then
-    tfcmt -var "target:${target}" plan -- terraform "$@"
+    tfcmt -var "target:${target}" plan -- tofu "$@"
 elif [ "$command" == "apply" ]; then
-    tfcmt -var "target:${target}" apply -- terraform "$@"
+    tfcmt -var "target:${target}" apply -- tofu "$@"
 else
-    terraform "$@"
+    tofu "$@"
 fi
