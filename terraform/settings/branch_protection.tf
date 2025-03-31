5e76d80f3ae156e861601588be2c4e7fe1a42712
Ryo Shimada <45233435+air-hand@users.noreply.github.com>
2025-04-01 07:44:02 +0900

    feat/sign commit by gpg (#1093)
    * feat: add gpg key for verify commits.
    
    * feat: allow commits with signs only.

--- terraform/settings/branch_protection.tf
+++ terraform/settings/branch_protection.tf
@@ -9,3 +9,36 @@ resource "github_branch_protection" "main" {
     strict = true
   }
 }
+
+resource "github_repository_ruleset" "main" {
+  name        = "main"
+  repository  = data.github_repository.current.name
+  target      = "branch"
+  enforcement = "active"
+
+  conditions {
+    ref_name {
+      include = [
+        "refs/heads/main",
+      ]
+      exclude = []
+    }
+  }
+
+  rules {
+    deletion                = true
+    non_fast_forward        = true
+    required_linear_history = true
+    required_signatures     = true
+
+    required_status_checks {
+      required_check {
+        context = "check-ci-result"
+      }
+      required_check {
+        context = "check-tf-result"
+      }
+      strict_required_status_checks_policy = true
+    }
+  }
+}
