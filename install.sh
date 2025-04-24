8f23ee3171b04517ae64fd4aef6a6d036668655b
air-hand <45233435+air-hand@users.noreply.github.com>
2025-04-24 20:53:43 +0900

    fix: specify sourceDir for test.

--- install.sh
+++ install.sh
@@ -23,6 +23,8 @@ fi
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 
+export MY_CHEZMOI_SOURCE=$script_dir
+
 set -- init --apply --source="${script_dir}"
 
 echo "Running 'chezmoi $*'" >&2
