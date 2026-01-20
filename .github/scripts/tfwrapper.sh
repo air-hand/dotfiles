4b1bd85b4d250d8d6605d0aac0c0cc73936dc99b
air-hand <45233435+air-hand@users.noreply.github.com>
2026-01-20 20:52:41 +0900

    chore: clean up tfwrapper.sh debug code, add TODO comment

--- .github/scripts/tfwrapper.sh
+++ .github/scripts/tfwrapper.sh
@@ -7,18 +7,11 @@ command=$1
 base_dir=$(git rev-parse --show-toplevel) # Please fix if necessary
 target=${PWD#"$base_dir"/}
 
+# TODO: tfcmt outputs "ERR tfcmt failed error=''" to stderr even on success (exit code 0)
 if [ "$command" == "plan" ]; then
-    set +e
-    tfcmt --log-level debug -var "target:${target}" plan -- terraform "$@"
-    exit_code=$?
-    echo "tfcmt exit code: $exit_code" >&2
-    exit $exit_code
+    tfcmt -var "target:${target}" plan -- terraform "$@"
 elif [ "$command" == "apply" ]; then
-    set +e
-    tfcmt --log-level debug -var "target:${target}" apply -- terraform "$@"
-    exit_code=$?
-    echo "tfcmt exit code: $exit_code" >&2
-    exit $exit_code
+    tfcmt -var "target:${target}" apply -- terraform "$@"
 else
     terraform "$@"
 fi
