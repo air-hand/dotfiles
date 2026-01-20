d71b9a094b99360dc464b13bcbef1991a3b5cc86
air-hand <45233435+air-hand@users.noreply.github.com>
2026-01-20 20:03:07 +0900

    debug: add tfcmt log-level debug

--- .github/scripts/tfwrapper.sh
+++ .github/scripts/tfwrapper.sh
@@ -8,9 +8,9 @@ base_dir=$(git rev-parse --show-toplevel) # Please fix if necessary
 target=${PWD#"$base_dir"/}
 
 if [ "$command" == "plan" ]; then
-    tfcmt -var "target:${target}" plan -- terraform "$@"
+    tfcmt --log-level debug -var "target:${target}" plan -- terraform "$@"
 elif [ "$command" == "apply" ]; then
-    tfcmt -var "target:${target}" apply -- terraform "$@"
+    tfcmt --log-level debug -var "target:${target}" apply -- terraform "$@"
 else
     terraform "$@"
 fi
