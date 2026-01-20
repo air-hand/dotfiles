cf302675ecce51d63cf80311e02e3b5968f12d5f
air-hand <45233435+air-hand@users.noreply.github.com>
2026-01-20 20:38:06 +0900

    debug: log tfcmt exit code

--- .github/scripts/tfwrapper.sh
+++ .github/scripts/tfwrapper.sh
@@ -8,9 +8,17 @@ base_dir=$(git rev-parse --show-toplevel) # Please fix if necessary
 target=${PWD#"$base_dir"/}
 
 if [ "$command" == "plan" ]; then
+    set +e
     tfcmt --log-level debug -var "target:${target}" plan -- terraform "$@"
+    exit_code=$?
+    echo "tfcmt exit code: $exit_code" >&2
+    exit $exit_code
 elif [ "$command" == "apply" ]; then
+    set +e
     tfcmt --log-level debug -var "target:${target}" apply -- terraform "$@"
+    exit_code=$?
+    echo "tfcmt exit code: $exit_code" >&2
+    exit $exit_code
 else
     terraform "$@"
 fi
