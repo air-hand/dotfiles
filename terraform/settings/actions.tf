f8af3db9aab289bd1d42f4418d48c722cc6c1231
air-hand <45233435+air-hand@users.noreply.github.com>
2026-04-14 18:13:23 +0900

    chore: import a github actions variable.

--- terraform/settings/actions.tf
+++ terraform/settings/actions.tf
@@ -3,3 +3,14 @@ resource "github_actions_variable" "tf_cloud_workspace" {
   variable_name = "TF_CLOUD_WORKSPACE"
   value         = "dotfiles"
 }
+
+resource "github_actions_variable" "gh_app_client_id" {
+  repository    = data.github_repository.current.name
+  variable_name = "GH_APP_CLIENT_ID"
+  value         = "Iv1.2fe071558871cd68"
+}
+
+import {
+  to = github_actions_variable.gh_app_client_id
+  id = "dotfiles:GH_APP_CLIENT_ID"
+}
